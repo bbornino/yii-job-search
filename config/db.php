e9bdbffe9882112645ea46f6b780086fb4ff9a16
Brian Bornino <brianbornino@gmail.com>
2024-04-28 12:25:17 -0700

    Use Correct Database

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=yii_job_search',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
